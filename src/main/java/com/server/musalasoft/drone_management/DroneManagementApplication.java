78c440a401ae65eb4d2a68c9965e0abba0950682
suren <surenanthonyvithanage@gmail.com>
2022-10-25 11:51:52 +0530

    base project

---
+++ src/main/java/com/server/musalasoft/drone_management/DroneManagementApplication.java
@@ -0,0 +1,13 @@
+package com.server.musalasoft.drone_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DroneManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DroneManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/server/musalasoft/drone_management/DroneManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.server.musalasoft.drone_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DroneManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
